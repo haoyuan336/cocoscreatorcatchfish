2a87e5b68f0d63f08ea59467823d34b075be5bbf
楚浩远 <328387458@qq.com>
2017-11-21 18:29:54 +0800

    none

--- catchfish/assets/scripts/fish.js
+++ catchfish/assets/scripts/fish.js
@@ -3,63 +3,18 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-       
+       spriteAtlas: {
+           default: null,
+           type: cc.SpriteAtlas
+       }
     },
 
     // use this for initialization
     onLoad: function () {
-        cc.loader.loadRes("./configs/fish-config", function(err, result){
-            let animates = result["fish_red"];
-            let animate = animates[0];
-            let animateName = animate.pre;
-            let spriteFrameNames = [];
-            for (let i = animate.start; i <= animate.end ; i ++) {
-                let name = animate.pre + '_' + i + '.png';
-                console.log("name = " + name);
-                spriteFrameNames.push(global.getSpriteFrameByName(name));
-            }
-            var clip = cc.AnimationClip.createWithSpriteFrames(spriteFrameNames, animate.speed);
-            this.node.getComponent(cc.Animation).addClip(clip, "run");
-            this.node.getComponent(cc.Animation).play();
-            
-    
-            
-        });
-        // let animates = config.animates;
-        // for (let i in animates){
-        //     let animate = animates[i];
-        //     let animateName = animate.pre;
-        //     let spriteFrameNames = [];
-        //     for (let i = animate.start; i <= animate.end ; i ++) {
-        //         let name = animate.pre + '_' + i + '.png';
-        //         console.log("name = " + name);
-        //         spriteFrameNames.push(global.getSpriteFrameByName(name));
-        //     }
-        //     var clip = cc.AnimationClip.createWithSpriteFrames(spriteFrameNames, animate.speed);
-        //     this.node.getComponent(cc.Animation).addClip(clip, "run");
-        //     this.node.getComponent(cc.Animation).play();
-        
 
-        // }
+
     },
     initWithData: function(config){
-        // console.log("init with data = " + JSON.stringify(config));
-        // var clip = cc.AnimationClip.createWithSpriteFrames()
-        let animates = config.animates;
-        for (let i in animates){
-            let animate = animates[i];
-            let animateName = animate.pre;
-            let spriteFrameNames = [];
-            for (let i = animate.start; i <= animate.end ; i ++) {
-                let name = animate.pre + '_' + i + '.png';
-                console.log("name = " + name);
-                spriteFrameNames.push(global.getSpriteFrameByName(name));
-            }
-            var clip = cc.AnimationClip.createWithSpriteFrames(spriteFrameNames, animate.speed);
-            this.node.getComponent(cc.Animation).addClip(clip, "run");
-            this.node.getComponent(cc.Animation).play();
-        
 
-        }
     }
 });
--- catchfish/assets/scripts/game-manager.js
+++ catchfish/assets/scripts/game-manager.js
@@ -1,7 +1,11 @@
 import global from './global'
+const AddFishState = {
+    Invalide: -1,
+    RandomFish: 1,
+    ArrayFish: 2,
+};
 cc.Class({
     extends: cc.Component,
-
     properties: {
       fishPrefab: {
           default: null,
@@ -13,31 +17,71 @@ cc.Class({
       }
     },
     onLoad: function () {
-        this.addFishTime = 0;
+        this.addRandomFishTime = 0;
+        this.stateAddFish = 0;
+        this.addFishStateTime = 0;
         cc.loader.loadRes("./configs/fish-config.json", (err, result)=>{
             this.fishConfig = result;
+            this.setAddFishState(AddFishState.RandomFish);
+
         });
-        cc.ff = {} || cc.ff;
-        global.getSpriteFrameByName = (name)=>{
-            return this.atlasFrames.getSpriteFrame(name);
-        };
+
+
+
     },
-    update: function(){
-        // if (this.fishConfig){
-        //     if (this.addFishTime > 1){
-        //         this.addFishTime = 0;
-        //         this.addFish(this.fishConfig[Object.keys(this.fishConfig)[0]]);
-        //     }else{
-        //         this.addFishTime ++;
-        //     }
-        // }
-       
+    update: function(dt){
+        this.addFishStateTime += dt;
+        switch (this.stateAddFish){
+            case AddFishState.RandomFish:
+                this.addRandomFish(dt);
+
+                if (this.addFishStateTime > 10){
+                    this.addFishStateTime = 0;
+                    this.setAddFishState(AddFishState.ArrayFish);
+                }
+                break;
+            case AddFishState.ArrayFish:
+
+                if (this.addFishStateTime > 10){
+                    this.addFishStateTime = 0;
+                    this.setAddFishState(AddFishState.RandomFish);
+                }
+                break;
+            default:
+                break;
+        }
+
     },
     addFish: function(config){
-        // console.log("add fish" + JSON.stringify(config));
-        let fishNode = cc.instantiate(this.fishPrefab);
-        fishNode.parent = this.node;
-        fishNode.getComponent("fish").initWithData(config);
+        // // console.log("add fish" + JSON.stringify(config));
+        // let fishNode = cc.instantiate(this.fishPrefab);
+        // fishNode.parent = this.node;
+        // fishNode.getComponent("fish").initWithData(config);
+    },
+    addRandomFish: function (dt) {
+        this.addRandomFishTime += dt;
+        if (this.addRandomFishTime > 1){
+            this.addRandomFishTime = 0;
+            //从配置里面 随机一条鱼，
+            let keys = Object.keys(this.fishConfig);
+            let fishType = keys[Math.floor(Math.random() * (keys.length - 1 ))];
+            cc.log('fish type = ' + fishType);
+
+        }
+    },
+    setAddFishState: function (state) {
+        if (this.stateAddFish === state){
+            return
+        }
+        switch (state){
+            case AddFishState.RandomFish:
+                break;
+            case AddFishState.ArrayFish:
+                break;
+            default:
+                break;
+        }
+        this.stateAddFish = state;
     }
 
 });
